5e223320d944c0ae6d94d5725b5c5c262d311ca5
Hansel <darkmastermindz@gmail.com>
2019-08-06 00:29:59 -0400

    Initialize Nav Component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
+    <app-nav />
     <nuxt />
   </div>
 </template>
+
+<script>
+import AppNav from '~/components/AppNav.vue'
+
+export default {
+  components: {
+    AppNav
+  },
+}
+</script>
+
 
 <style>
 html {
