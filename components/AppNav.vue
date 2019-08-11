0737e2a2759b770fc3e88cf4198544376225c3b1
Hansel <darkmastermindz@gmail.com>
2019-08-11 18:01:20 -0400

    Add Nav for Projects

---
+++ components/AppNav.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+<ul>
+    <li>
+        <nuxt-link exact to="/">Home</nuxt-link>
+    </li>
+    <li>
+        <nuxt-link exact to="/Projects">Projects</nuxt-link>
+    </li>
+</ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100%;
+
+    ul{
+        padding-left: 0;
+        li {
+            display: inline-block;
+            margin-right: 20px;
+            
+        }
+    }
+}
+</style>
