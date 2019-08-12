e8f3a0b623bdff7c62284cf877786bfb2eca09ff
Hansel Wei <hello@hanselwei.dev>
2019-08-11 21:59:25 -0400

    Add Dark Theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,6 +27,14 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  background-color:#2c2f33;
+}
+
+h1, h2, h3 {
+  font-family: 'Nova Mono', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+    font-weight: normal;
+    color: #eee;
 }
 
 *,
@@ -36,32 +44,34 @@ html {
   margin: 0;
 }
 
+
 .button--green {
   display: inline-block;
   border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+  border: 1px solid #00ff9f;
+  color: #00ff9f;
   text-decoration: none;
   padding: 10px 30px;
 }
 
 .button--green:hover {
   color: #fff;
-  background-color: #3b8070;
+  background-color: #00ff9f;
 }
 
 .button--grey {
   display: inline-block;
   border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
+  border: 1px solid #fff ;
+  color: #fff ;
   text-decoration: none;
   padding: 10px 30px;
   margin-left: 15px;
 }
 
 .button--grey:hover {
+  border: 1px solid #171515;
   color: #fff;
-  background-color: #35495e;
+  background-color: #171515;
 }
 </style>
