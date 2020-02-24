76bfcec5c99a97b360395d8dbb30d975cd073a23
Hansel Wei <hello@hanselwei.dev>
2020-02-24 03:12:37 -0500

    Update AppNav.vue

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,17 +1,9 @@
 <template>
-  <div>
-    <ul>
-      <li>
-        <nuxt-link exact to="/">🏠📜 (H)</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link exact to="/Projects">🐙👨🏻‍💻 (P)</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link exact to="/StayCurious">📝📖 (J)</nuxt-link>
-      </li>
-    </ul>
-  </div>
+    <nav class="pa3 pa4-ns">
+        <nuxt-link class="link dim white b f6 f5-ns dib mr3" exact to="/">🏠📜 Home</nuxt-link>
+        <nuxt-link class="link dim blue b f6 f5-ns dib mr3" exact to="/Projects">🐙👨🏻‍💻 Projects</nuxt-link>
+        <nuxt-link class="link dim green b f6 f5-ns dib mr3" exact to="/StayCurious">📝📖 Newsletter</nuxt-link>
+    </nav>
 </template>
 
 <script>
@@ -19,33 +11,76 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-div {
-  display: flex;
-  justify-content: center;
-  align-items: top;
-  flex-shrink: inherit;
-  width: 100vw;
-  height: 100px;
-
-  ul {
-    li {
-      margin: 0.5em;
-      border-radius: 0.75em;
-      background-color: aliceblue;
-      padding: 1em;
-      position: center;
-      overflow-x: hidden;
-      z-index: 1;
-
-      display: inline;
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-
-      a {
-        text-decoration: none;
-      }
-    }
-  }
+.dib {
+        display: inline-block;
 }
+.b {
+        font-weight: bold;
+}
+.link {
+        text-decoration: none;
+        transition: color .15s ease-in;
+        &:link {
+                transition: color .15s ease-in;
+        }
+        &:visited {
+                transition: color .15s ease-in;
+        }
+        &:hover {
+                transition: color .15s ease-in;
+        }
+        &:active {
+                transition: color .15s ease-in;
+        }
+        &:focus {
+                transition: color .15s ease-in;
+                outline: 1px dotted currentColor;
+        }
+}
+.blue {
+    color: #00b8ff;
+}
+.black {
+        color: #000;
+}
+.gray {
+        color: #777;
+}
+.green {
+    color: #41B883;
+}
+.pa3 {
+        padding: 1rem;
+}
+.mr3 {
+        margin-right: 1rem;
+}
+.f6 {
+        font-size: .875rem;
+}
+.dim {
+        opacity: 1;
+        transition: opacity .15s ease-in;
+        &:hover {
+                opacity: .5;
+                transition: opacity .15s ease-in;
+        }
+        &:focus {
+                opacity: .5;
+                transition: opacity .15s ease-in;
+        }
+        &:active {
+                opacity: .8;
+                transition: opacity .15s ease-out;
+        }
+}
+@media screen and (min-width: 30em) {
+        .pa4-ns {
+                padding: 2rem;
+        }
+        .f5-ns {
+                font-size: 1rem;
+        }
+}
+
 </style>
